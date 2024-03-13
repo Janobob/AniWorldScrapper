f19956c9ae87d2d3777d45fbe404d832b6ea6c6a
Jan Werren <jan.werren@outlook.com>
2024-03-13 20:36:52 +0100

    feat: generate links for multiple episodes

--- Program.cs
+++ Program.cs
@@ -2,47 +2,56 @@
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
 
-var extensionPath = @"C:\Users\janwe\AppData\Local\Google\Chrome\User Data\Default\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm\1.56.0_0";
+const string extensionPath = @"C:\Users\janwe\AppData\Local\Google\Chrome\User Data\Default\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm\1.56.0_0";
+const string animeUrl = "https://aniworld.to/anime/stream/classroom-of-the-elite/staffel-1/episode-1";
+
+// Set up the Chrome driver
 var options = new ChromeOptions();
 options.AddArgument($"load-extension={extensionPath}");
+options.AddExcludedArguments("excludeSwitches", "enable-logging");
 var driver = new ChromeDriver(options);
+driver.Url = animeUrl;
 
-driver.Url = "https://aniworld.to/anime/stream/classroom-of-the-elite/staffel-1/episode-1";
 
-var linkElement = driver.FindElements(By.CssSelector("[data-link-id] a"))[3];
-var href = linkElement.GetAttribute("href");
+// Sleep to prevent ads
+Thread.Sleep(1000);
 
-Console.WriteLine(href);
-//
-// PerformClick();
-// PerformClick();
-//
-// var video = driver.FindElement(By.TagName("video")).GetAttribute("src");
-//
-// Console.WriteLine(video);
-//
-// return;
-//
-//
-// void PerformClick()
-// {
-// 	var actions = new Actions(driver);
-// 	actions.Click(linkElement).Perform();
-//
-// 	Thread.Sleep(1000);
-//
-// 	try
-// 	{
-// 		driver.SwitchTo().Window(driver.WindowHandles[1]);
-// 		driver.Close();
-// 		driver.SwitchTo().Window(driver.WindowHandles[0]);
-// 	}
-// 	catch
-// 	{
-// 		// ignored
-// 	}
-// }
-//
+var episodeLinks = driver.FindElements(By.CssSelector("[data-episode-id]"));
+var activeEpisode = episodeLinks.IndexOf(episodeLinks.First(e => e.GetAttribute("class").Contains("active")));
 
+for (var episode = activeEpisode; episode < episodeLinks.Count - 1; episode++)
+{
+	// run
+	GetVideoForEpisode();
+	
+	// get links
+	episodeLinks = driver.FindElements(By.CssSelector("[data-episode-id]"));
+	episodeLinks[episode + 1].Click();
+}
+
+driver.Quit();
+
+return;
+
+void GetVideoForEpisode()
+{
+	var linkElement = driver.FindElements(By.CssSelector("[data-link-id] a"))[3];
+	var href = linkElement.GetAttribute("href");
+
+	((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+	driver.SwitchTo().Window(driver.WindowHandles.Last());
+	driver.Navigate().GoToUrl(href);
+
+	Thread.Sleep(2000);
+
+	driver.FindElement(By.TagName("body")).Click();
+	((IJavaScriptExecutor)driver).ExecuteScript("document.getElementsByClassName('play-overlay')[0].click();");
+	var control = driver.FindElement(By.ClassName("plyr__control"));
+	control.Click();
+	var video = driver.FindElement(By.TagName("video")).GetAttribute("src");
+	Console.WriteLine(video);
+
+	driver.Close();
+	driver.SwitchTo().Window(driver.WindowHandles.First());
+}
