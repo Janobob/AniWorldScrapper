5c44042e42e2d74980fc1ef482481df0ca40b03d
Jan Werren <jan.werren@outlook.com>
2024-04-21 15:49:40 +0200

    feat: add renamer

---
+++ AniWorldScrapper.Renamer/Renamer.cs
@@ -0,0 +1,39 @@
+﻿namespace AniWorldScrapper.Renamer;
+
+public class Renamer
+{
+	public static void Main(string[] args)
+	{
+		// Directory containing files
+		const string directoryPath = @"\\192.168.1.176\Plex\downloads";
+		const string template = "MASHLE - MAGIC AND MUSCLES S{0:D2}E{1:D2}";
+		var season = 1;
+		var episode = 7;
+
+		try
+		{
+			// Get all files in the directory
+			var files = Directory.GetFiles(directoryPath);
+
+			foreach (var filePath in files)
+			{
+				// Get file name and extension
+				var fileName = Path.GetFileName(filePath);
+				var fileExtension = Path.GetExtension(filePath);
+
+				// Rename the file
+				var newFileName = string.Format(template, season, episode) + fileExtension;
+				var newFilePath = Path.Combine(directoryPath, newFileName);
+				File.Move(filePath, newFilePath);
+
+				// Output the old and new file names
+				Console.WriteLine($"Renamed: {fileName} -> {newFileName}");
+				episode++;
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"An error occurred: {ex.Message}");
+		}
+	}
+}
